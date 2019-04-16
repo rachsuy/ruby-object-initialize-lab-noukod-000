5fd12d83e30b3601975924a3a53d465880effb90
Rachele Suy <rachelesuy@gmail.com>
2019-04-16 13:47:22 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog 
+  
+  def initialize(dog_namme, dog_breed )
+    @breed = breed
+    @name = name
+ 
+  end
+  
+def name=(name, breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
+  end
+  
+  
+  def breed
+    @breed
+  end
+  
+  def name 
+    @name
+  end
+end
+
